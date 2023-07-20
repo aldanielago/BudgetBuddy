875b8f0c894add48aac2c6ed18c06db33abdb090
aldanielago <aldanielago@gmail.com>
2023-07-20 13:00:58 -0500

    [Estilos] Paleta de colores y primera seccion

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,32 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': '#52796f',
-        'secondary-light': '#84a98c',
-        'secondary-dark': '#354f52',
-        'back-light': '#cad2c5',
-        'back-dark': '#2f3e46',
+        // Light Theme
+        'light-primary-100': '#0077C2',
+        'light-primary-200': '#59a5f5',
+        'light-primary-300': '#c8ffff',
+        'light-acent-100': '#00BFFF',
+        'light-acent-200': '#00619a',
+        'light-text-100': '#333333',
+        'light-text-200': '#5c5c5c',
+        'light-bg-100': '#FFFFFF',
+        'light-bg-200': '#f5f5f5',
+        'light-bg-300': '#cccccc',
+
+        // Dark Theme
+        'dark-primary-100': '#0085ff',
+        'dark-primary-200': '#69b4ff',
+        'dark-primary-300': '#e0ffff',
+        'dark-acent-100': '#006fff',
+        'dark-acent-200': '#e1ffff',
+        'dark-text-100': '#FFFFFF',
+        'dark-text-200': '#9e9e9e',
+        'dark-bg-100': '#1E1E1E',
+        'dark-bg-200': '#2d2d2d',
+        'dark-bg-300': '#454545'
+      },
+      fontFamily: {
+        'Quicksand': ['Quicksand', 'sans-serif'],
       }
     },
   },
