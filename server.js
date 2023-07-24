62cbcf2bda44eaba2f90b2d933da2fe060c271b2
aldanielago <aldanielago@gmail.com>
2023-07-23 22:12:35 -0500

    [Estilos] Sección login

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express  = require('express');
 const router_general = require('./router/index');
-const bodyParser = require('body-parser');
 const port = 3000;
 
 const app = express();
@@ -9,14 +8,11 @@ const path = require('path');
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500'); // Reemplaza esta URL con la del cliente
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
-
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'view')));
 
--- view/js/navegation.js
+++ view/js/navegation.js
@@ -1,59 +1,59 @@
 function signUpPage() {
-  section_sign_up.classList.remove('invisible')
-  section_log_in.classList.add('invisible')
-  section_registro_ingresos.classList.add('invisible')
-  section_registro_egresos.classList.add('invisible')
-  section_registro_presupuesto.classList.add('invisible')
-  section_consulta_ingresos.classList.add('invisble')
-  section_consulta_egresos.classList.add('invisible')
+  section_sign_up.classList.remove('hidden')
+  section_log_in.classList.add('hidden')
+  section_registro_ingresos.classList.add('hidden')
+  section_registro_egresos.classList.add('hidden')
+  section_registro_presupuesto.classList.add('hidden')
+  section_consulta_ingresos.classList.add('hidden')
+  section_consulta_egresos.classList.add('hidden')
 }
 
 function loginPage() {
-  section_sign_up.classList.add('invisible')
-  section_log_in.classList.remove('invisible')
-  section_registro_ingresos.classList.add('invisible')
-  section_registro_egresos.classList.add('invisible')
-  section_registro_presupuesto.classList.add('invisible')
-  section_consulta_ingresos.classList.add('invisble')
-  section_consulta_egresos.classList.add('invisible')
+  section_sign_up.classList.add('hidden')
+  section_log_in.classList.remove('hidden')
+  section_registro_ingresos.classList.add('hidden')
+  section_registro_egresos.classList.add('hidden')
+  section_registro_presupuesto.classList.add('hidden')
+  section_consulta_ingresos.classList.add('hidden')
+  section_consulta_egresos.classList.add('hidden')
 }
 
 function registroIngresoPage(){
-  section_sign_up.classList.add('invisible')
-  section_log_in.classList.add('invisible')
-  section_registro_ingresos.classList.remove('invisible')
-  section_registro_egresos.classList.add('invisible')
-  section_registro_presupuesto.classList.add('invisible')
-  section_consulta_ingresos.classList.add('invisble')
-  section_consulta_egresos.classList.add('invisible')
+  section_sign_up.classList.add('hidden')
+  section_log_in.classList.add('hidden')
+  section_registro_ingresos.classList.remove('hidden')
+  section_registro_egresos.classList.add('hidden')
+  section_registro_presupuesto.classList.add('hidden')
+  section_consulta_ingresos.classList.add('hidden')
+  section_consulta_egresos.classList.add('hidden')
 }
 
 function registroEgresoPage() {
-  section_sign_up.classList.add('invisible')
-  section_log_in.classList.add('invisible')
-  section_registro_ingresos.classList.add('invisible')
-  section_registro_egresos.classList.remove('invisible')
-  section_registro_presupuesto.classList.add('invisible')
-  section_consulta_ingresos.classList.add('invisble')
-  section_consulta_egresos.classList.add('invisible')
+  section_sign_up.classList.add('hidden')
+  section_log_in.classList.add('hidden')
+  section_registro_ingresos.classList.add('hidden')
+  section_registro_egresos.classList.remove('hidden')
+  section_registro_presupuesto.classList.add('hidden')
+  section_consulta_ingresos.classList.add('hidden')
+  section_consulta_egresos.classList.add('hidden')
 }
 
 function registroPresupuestoPage() {
-  section_sign_up.classList.add('invisible')
-  section_log_in.classList.add('invisible')
-  section_registro_ingresos.classList.add('invisible')
-  section_registro_egresos.classList.add('invisible')
-  section_registro_presupuesto.classList.remove('invisible')
-  section_consulta_ingresos.classList.add('invisble')
-  section_consulta_egresos.classList.add('invisible')
+  section_sign_up.classList.add('hidden')
+  section_log_in.classList.add('hidden')
+  section_registro_ingresos.classList.add('hidden')
+  section_registro_egresos.classList.add('hidden')
+  section_registro_presupuesto.classList.remove('hidden')
+  section_consulta_ingresos.classList.add('hidden')
+  section_consulta_egresos.classList.add('hidden')
 }
 
 function principalPage() {
-  section_sign_up.classList.add('invisible');
-  section_log_in.classList.add('invisible');
-  section_registro_ingresos.classList.add('invisible');
-  section_registro_egresos.classList.add('invisible');
-  section_registro_presupuesto.classList.add('invisible');
-  section_consulta_ingresos.classList.remove('invisible');
-  section_consulta_egresos.classList.remove('invisible');
+  section_sign_up.classList.add('hidden');
+  section_log_in.classList.add('hidden');
+  section_registro_ingresos.classList.add('hidden');
+  section_registro_egresos.classList.add('hidden');
+  section_registro_presupuesto.classList.add('hidden');
+  section_consulta_ingresos.classList.remove('hidden');
+  section_consulta_egresos.classList.remove('hidden');
 }
