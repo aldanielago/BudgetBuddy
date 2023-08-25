d5c15f5bb0c9860149962637b67b4d5ae55f694e
aldanielago <aldanielago@gmail.com>
2023-08-25 15:57:16 -0500

    [Refactorización]

--- view/js/nodes.js
+++ view/js/nodes.js
@@ -1,6 +1,26 @@
 // No olvides poner los puntitos para evitar dolores de cabeza por fa
 const $ = (id) => document.querySelector(id);
 
+const Toast = Swal.mixin({
+	toast: true,
+	showConfirmButton: false,
+	timer: 2000,
+	timerProgressBar: true,
+	didOpen: (toast) => {
+		toast.addEventListener("mouseenter", Swal.stopTimer);
+		toast.addEventListener("mouseleave", Swal.resumeTimer);
+	},
+});
+
+// Botones de navegación
+const btn_sign_up_registrar = $('#sign_up_registrar');
+const btn_sign_up_login = $('#sign_up_iniciar');
+const btn_log_in_registrar = $('#log_in_registrar');
+const btn_log_in_login = $('#log_in_iniciar');
+const btn_registrar_ingreso = $('#registrar_ingreso');
+const btn_registrar_egreso = $('#registrar_egreso');
+const btn_registrar_presupuesto = $('#registrar_presupuesto');
+
 // Para el sign up
 const section_sign_up = $('#section_sign_up');
 
